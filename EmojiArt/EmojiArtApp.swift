edf12b660da68c6cf4cb32480b00864c311e92da
Austin Zheng <austin.zheng@ringcentral.com>
2020-12-23 10:11:22 +0800

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Austin Zheng on 2020/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
